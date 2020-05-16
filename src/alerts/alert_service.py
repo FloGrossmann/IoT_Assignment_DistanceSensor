d2d3781cb5c2aa1ce6ecc60e616a94dd42ead697
FloGrossmann <florian4774@gmail.com>
2020-05-16 15:00:02 +0200

    subscribed to broker

--- src/alerts/alert_service.py
+++ src/alerts/alert_service.py
@@ -4,16 +4,20 @@ class AlertService: # Service Reagiert auf Messwerte
     alertDistance = 30
 
     #  Initialisierung
-    def __init__(self, startDistance):
+    def __init__(self, startDistance, broker):
+        self.broker = broker
+        broker.sub("iot-distance-sensor/data", self.checkAlert)
         AlertService.alertDistance = startDistance
-        pass
 
     #  value:  Neuer Schwellwert als einfache Zahl in der Einheit cm wird hier übergeben
     def set_alert_threshold (self, value):
         AlertService.alertDistance = value
-        pass
 
     #  value:  Aktuell gemessener Wert (siehe DistanceSensor) wird übergeben und es wird ein Alarm gesendet
     def on_distance_threshold_passed(self, value):
         print("Alert!! Distance is less than: ", AlertService.alertDistance)
-        pass
+        self.broker.publish("iot-distance-sensor/alarm", value)
+
+    def checkAlert(self, dictRead):
+        if int(dictRead["distance"]) < int(AlertService.alertDistance):
+            self.on_distance_threshold_passed(dictRead)
